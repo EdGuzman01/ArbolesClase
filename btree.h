222b805c19de2a3c4cc83d24b86a368532ff030d
EdGuzman01 <00371023@uca.edu.sv>
2024-10-22 16:54:36 -0600

    feat: Agregar archivo .h

--- btree.h
+++ btree.h
@@ -1,6 +1,28 @@
-crear una estructura que permita almacenar  enteros en un AB 
-los PARES D, los impares I 
+#include <iostream>
+#include <set> // --> Libreria para utilizar arboles
 
-crear una .h y .cpp con las funciones
+struct Node {
+  int data;
+  Node* left;
+  Node* right;
+  Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
 
-LAS FUNCIONES insertar, eliminar y buscar
+class BTree {
+public:
+  BTree();
+  ~BTree();
+  
+  void insert(int value);
+  bool search(int value);
+  void remove(int value); 
+
+private:
+  Node* root;
+
+  Node* insert(Node* node, int value);
+  Node* search(Node* node, int value);
+  Node* remove(Node* node, int value);
+  Node* findMin(Node* node);
+  void destroyTree(Node* node);
+};
