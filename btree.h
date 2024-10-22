534d219c557da32b5f0d08bf0f9a7dd158b6efc0
EdGuzman01 <00371023@uca.edu.sv>
2024-10-22 17:06:56 -0600

    fix: se cambio el avance del archivo .h

--- btree.h
+++ btree.h
@@ -7,22 +7,3 @@ struct Node {
   Node* right;
   Node(int val) : data(val), left(nullptr), right(nullptr) {}
 };
-
-class BTree {
-public:
-  BTree();
-  ~BTree();
-  
-  void insert(int value);
-  bool search(int value);
-  void remove(int value); 
-
-private:
-  Node* root;
-
-  Node* insert(Node* node, int value);
-  Node* search(Node* node, int value);
-  Node* remove(Node* node, int value);
-  Node* findMin(Node* node);
-  void destroyTree(Node* node);
-};
