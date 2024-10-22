3156ebddb454fd2ff96b052590a8de893cde2d71
EdGuzman01 <00371023@uca.edu.sv>
2024-10-22 17:12:28 -0600

    feat: Agregar estructura para el archivo .h

--- btree.h
+++ btree.h
@@ -1,9 +1,15 @@
-#include <iostream>
-#include <set> // --> Libreria para utilizar arboles
+#ifndef BTREE_H
+#define BTREE_H
 
 struct Node {
-  int data;
-  Node* left;
-  Node* right;
-  Node(int val) : data(val), left(nullptr), right(nullptr) {}
+    int data;
+    Node* left;
+    Node* right;
 };
+
+Node* createNode(int value);
+Node* insert(Node* node, int value);
+void display(Node* node);
+void destroyTree(Node* node);
+
+#endif // BTREE_H
